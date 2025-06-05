2d176522d560e65446174b0345e5379b6cc7a2c0
aapani-dukan <shivraj8404@gmail.com>
2025-06-05 19:54:03 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
-
+// drizzle.config.ts
 import { defineConfig } from "drizzle-kit";
-import 'dotenv/config';
+import * as dotenv from "dotenv";      // ✅ ये ज़रूरी है
+dotenv.config();                       // ✅ इससे .env file load होती है
+
 export default defineConfig({
   schema: "./shared/schema.ts",
   out: "./drizzle/migrations",
   dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
