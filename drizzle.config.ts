d1567c4fe1232613abeaa88974e7259fc47ae54c
aapani-dukan <shivraj8404@gmail.com>
2025-06-05 19:32:41 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 export default defineConfig({
   schema: './server/shared/schema.ts', // या आपका actual schema path
-  out: './server/migrations',
+  out: './drizzle/migrations',
   driver: 'pg',
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
