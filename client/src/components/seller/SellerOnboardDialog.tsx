5109668ddb6dfd7944a10699dae236b268bd9c7c
aapani-dukan <shivraj8404@gmail.com>
2025-06-06 23:39:54 +0530

    Update SellerOnboardDialog.tsx

--- client/src/components/seller/SellerOnboardDialog.tsx
+++ client/src/components/seller/SellerOnboardDialog.tsx
@@ -1,7 +1,7 @@
 // components/seller-onboarding-dialog.tsx
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-
+import SellerRegistrationForm from "./SellerRegistrationForm";
 interface SellerOnboardingDialogProps {
   open: boolean;
   onClose: () => void;
