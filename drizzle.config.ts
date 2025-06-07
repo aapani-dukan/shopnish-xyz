84890900ca00bf5fa4ff1d14b5fa8dc5597b753d
aapani-dukan <shivraj8404@gmail.com>
2025-06-07 10:59:43 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: './server/shared/schema.ts',
+  schema: './shared/schema.ts',
   out: './server/migrations',
   dialect: 'postgresql',    // postgres के लिए यह सेट करें
   dbCredentials: {
