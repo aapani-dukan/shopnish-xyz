6bdfeca71dfe9c294fba76d1f87266c4c13abf58
aapani-dukan <shivraj8404@gmail.com>
2025-06-07 11:26:02 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,6 +1,6 @@
-import 'dotenv/config';
+require('dotenv').config();
 
-export default {
+module.exports = {
   schema: './shared/schema.ts',
   out: './server/migrations',
   dialect: 'postgresql',
