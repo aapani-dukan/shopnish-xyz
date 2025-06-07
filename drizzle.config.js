cc41806913cec8efcb048a3763db3deb46d5dd56
aapani-dukan <shivraj8404@gmail.com>
2025-06-07 12:49:55 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('drizzle-kit');
 module.exports = defineConfig({
   schema: './shared/schema.ts',
   out: './server/migrations',
-  dialect: 'postgresql',
+  dialect: 'pg', // 🔴 यही सबसे जरूरी बदलाव है
   dbCredentials: {
     connectionString: 'postgresql://neondb_owner:npg_98LZoDWjpxkK@ep-falling-mud-a8ib7jxz-pooler.eastus2.azure.neon.tech/neondb?sslmode=require',
   },
