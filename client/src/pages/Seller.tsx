53c5746b86a96af79751283cde902506430fc3ed
aapani-dukan <shivraj8404@gmail.com>
2025-06-08 13:12:18 +0530

    Update Seller.tsx

--- client/src/pages/Seller.tsx
+++ client/src/pages/Seller.tsx
@@ -1,1 +1,23 @@
+// pages/seller.tsx
+import { useAuth } from "@/hooks/useAuth";
+import SellerDashboard from "@/components/seller/SellerDashboard";
+import SellerRegistrationModal from "@/components/modals/seller-registration-modal";
+import Loader from "@/components/shared/loader";
 
+export default function SellerGate() {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!user) {
+    return <div>Please sign in to continue</div>;
+  }
+
+  if (!user.seller) {
+    return <SellerRegistrationModal />;
+  }
+
+  return <SellerDashboard seller={user.seller} />;
+}
