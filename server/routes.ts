0b2b8ae90db8f25a404f0ac11cd4d11af0e09027
aapani-dukan <shivraj8404@gmail.com>
2025-06-09 08:50:48 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -10,7 +10,7 @@ import sellersApplyRouter from "../routes/sellers/apply";
 import sellersApproveRouter from "../routes/sellers/approve";
 import sellersRejectRouter from "../routes/sellers/reject";
 import sellerMeRoute from "../routes/sellerMe";
-
+import authRoutes from "./routes/authRoutes";
 export async function registerRoutes(app: Express): Promise<Server> {
   // Seed database on startup
   try {
