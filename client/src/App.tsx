80596c87ac1c6d3279a82abb2f2742c9138ec31a
aapani-dukan <shivraj8404@gmail.com>
2025-06-10 22:23:59 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { SellerRegistrationForm } from "@/components/seller/SellerRegistrationForm";
+import { RegisterSellerPage } from "@/pages/register-seller";
 import Home from "@/pages/home";
 import ProductDetail from "@/pages/product-detail";
 import Cart from "@/pages/cart";
