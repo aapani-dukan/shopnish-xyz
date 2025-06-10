4e150939ef32c5a6917517ba6eb19e4122d1e689
aapani-dukan <shivraj8404@gmail.com>
2025-06-10 11:06:54 +0530

    Update register-seller.tsx

--- client/src/pages/register-seller.tsx
+++ client/src/pages/register-seller.tsx
@@ -1,20 +1,9 @@
-// pages/register-seller.tsx
-import { useEffect } from "react";
-import { useLocation } from "wouter";
-import Loader from "../../../shared/Loader";
 import { useAuth } from "@/hooks/useAuth";
-
 import SellerRegistrationModal from "@/components/seller-registration-modal";
+import Loader from "../../../shared/Loader";
 
 export default function RegisterSellerPage() {
   const { user, loading } = useAuth();
-  const [, navigate] = useLocation();
-
-  useEffect(() => {
-    if (!loading && !user) {
-      navigate("/login");
-    }
-  }, [user, loading, navigate]);
 
   if (loading || !user) {
     return <Loader />;
