424c77caf113caa9c3fb6a052c74795bc0ebecaa
aapani-dukan <shivraj8404@gmail.com>
2025-06-14 21:11:28 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useAuth } from "@/hooks/useAuth";
 
-import Landing    from "@/pages/Landing";
+import Landing    from "@/pages/landing";
 import Dashboard  from "@/pages/Dashboard";
 
 export default function App() {
