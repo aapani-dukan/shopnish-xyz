a694bee56ea12caec14d411e0d34d5a71cfc8eaf
aapani-dukan <shivraj8404@gmail.com>
2025-06-14 21:34:59 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,12 @@
-import ReactDOM from "react-dom/client";
-import App from "./App";
+// src/main.tsx
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+// import './index.css' // Agar aapke paas global CSS hai toh uncomment kar sakte hain
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
