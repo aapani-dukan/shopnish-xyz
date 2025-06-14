46605f1b7ee1b9fe4f7eb0af93393d4998927a98
aapani-dukan <shivraj8404@gmail.com>
2025-06-14 20:21:56 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/Landing";
+import LandingPage from "./pages/landing";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
