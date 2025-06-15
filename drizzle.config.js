fbac9bb6571decd7bea3fa3c16ae2f7fd82f82a5
aapani-dukan <shivraj8404@gmail.com>
2025-06-15 11:15:19 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,1 +1,11 @@
 
+const { defineConfig } = require('drizzle-kit');
+
+module.exports = defineConfig({
+  schema: './shared/backend/schema.ts',
+  out: './server/migrations',
+  dialect: 'postgresql',  // ✅ यह line ज़रूरी है!
+  dbCredentials: {
+    url: 'postgresql://shopnishu_user:LDaO1MrBi45i7kVzcrU2VekEkNkeJUKH@dpg-d16qqtgdl3ps739nvv90-a.oregon-postgres.render.com/shopnish_db',
+  },
+});
