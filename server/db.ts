4d231aa051ecb9268b6d8331bc54d66989c17f4e
aapani-dukan <shivraj8404@gmail.com>
2025-06-15 13:56:17 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,15 +1,17 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
-import ws from "ws";
-import * as schema from "@shared/backend/schema";
+// instead of 'neon-serverless' and websocket
+import { Client } from 'pg';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from '@shared/backend/schema';
 import 'dotenv/config';
-neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  throw new Error('DATABASE_URL must be set');
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle({ client: pool, schema });
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+await client.connect();
+
+export const db = drizzle(client, { schema });
