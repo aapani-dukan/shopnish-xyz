dbaa51d8facb320adae6209e5963c2369512fcbc
aapani-dukan <shivraj8404@gmail.com>
2025-06-15 18:11:29 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,10 +1,14 @@
-const { defineConfig } = require('drizzle-kit');
+ror("DATABASE_URL, ensure the database is provisioned");
+};
 
-module.exports = defineConfig({
-  schema: './shared/backend/schema.ts',
-  out: './server/migrations',
-  dialect: 'postgresql',
+if (!process.env.DATABASE_URL) {
+  throw new Er
+
+export default defineConfig({
+  out: "./miimport { defineConfig } from "drizzle-kit"grations",
+  schema: "./shared/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL, // ✅ Safe
+    url: process.env.DATABASE_URL,
   },
 });
