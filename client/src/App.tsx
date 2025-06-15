e78f8e55b3c97fc5694e291f784fd6ca9097d4f5
aapani-dukan <shivraj8404@gmail.com>
2025-06-15 18:25:48 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // src/App.tsx
-import { Switch, Route } from "wouter";
+import { Switch, Route,Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
