e8d2b46c184de0cd4806a72414e61c2cc264e720
aapani-dukan <shivraj8404@gmail.com>
2025-06-16 19:27:26 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,6 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
