3627b4ecca66dbff3abe64c2f5e8db240a5237c0
aapani-dukan <shivraj8404@gmail.com>
2025-06-18 09:03:46 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("❌ Root element not found. Please check index.html");
-}
+createRoot(document.getElementById("root")!).render(<App />);
+           
