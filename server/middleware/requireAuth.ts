1b69579fb03311c817a15d45eb30f495ed898e1f
aapani-dukan <shivraj8404@gmail.com>
2025-06-20 18:53:59 +0530

    Create requireAuth.ts

---
+++ server/middleware/requireAuth.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Response } from "express";
+import { AuthenticatedRequest } from "./verifyToken";
+
+export function requireAuth(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  if (!req.user || !req.user.id) {
+    return res.status(401).json({ message: "User not authenticated." });
+  }
+  next();
+}
