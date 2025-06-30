8c650876b5c0bbcf11eb167dfe53cdfa25fdbeb3
aapani-dukan <shivraj8404@gmail.com>
2025-06-30 18:00:24 +0530

    Update authMiddleware.ts

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -1,7 +1,7 @@
 // server/middleware/authMiddleware.ts
 import { Response, NextFunction } from 'express';
 import { verifyToken, AuthenticatedRequest } from './verifyToken.js'; // Import verifyToken and AuthenticatedRequest
-import { userRoleEnum } from '@/shared/backend/schema.js'; // Import userRoleEnum
+import { userRoleEnum } from '@/shared/backend/schema'; // Import userRoleEnum
 
 // Middleware for general authentication
 export const requireAuth = (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
