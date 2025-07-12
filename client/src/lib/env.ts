c9a088651c36041649c8ec91b153d0373bd9a5a5
aapani-dukan <shivraj8404@gmail.com>
2025-07-12 15:36:27 +0530

    Update env.ts

--- client/src/lib/env.ts
+++ client/src/lib/env.ts
@@ -1,10 +1,12 @@
 // lib/env.ts
-
 export const firebaseConfig = {
   apiKey: "AIzaSyChdYrxfxkfj6m04WT0nOBl5xCP62udcPU",
   authDomain: "aapani-dukan.firebaseapp.com",
+  databaseURL: "https://aapani-dukan.firebaseio.com",
   projectId: "aapani-dukan",
   storageBucket: "aapani-dukan.firebasestorage.app",
   messagingSenderId: "352463214204",
   appId: "1:352463214204:web:a3adc9ef1d8af0de1fdbf9"
 };
+
+export const API_BASE = "https://shopnish-9vlk.onrender.com"; // ✅ Safe
