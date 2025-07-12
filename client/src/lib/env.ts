597f2d3c27fc849a9ab029a9d5dbd055a3d02267
aapani-dukan <shivraj8404@gmail.com>
2025-07-12 15:27:19 +0530

    Create env.ts

---
+++ client/src/lib/env.ts
@@ -0,0 +1,10 @@
+// lib/env.ts
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyChdYrxfxkfj6m04WT0nOBl5xCP62udcPU",
+  authDomain: "aapani-dukan.firebaseapp.com",
+  projectId: "aapani-dukan",
+  storageBucket: "aapani-dukan.firebasestorage.app",
+  messagingSenderId: "352463214204",
+  appId: "1:352463214204:web:a3adc9ef1d8af0de1fdbf9"
+};
