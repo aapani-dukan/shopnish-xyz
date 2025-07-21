c4e2566c8746551c6564e00a8763b42cadabedd6
aapani-dukan <shivraj8404@gmail.com>
2025-07-21 16:50:45 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Router import
 import App from "./App.tsx";
-import "./index.css"; // ग्लोबल CSS इम्पोर्ट करें
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>         {/* ✅ Router wrapper */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
