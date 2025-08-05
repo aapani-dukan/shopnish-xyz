efd71637503c4f1e36a92801dbd6e6a1d6da889c
aapani-dukan <shivraj8404@gmail.com>
2025-08-05 21:52:12 +0530

    Delete drizzle/migrations/0001_final_auth_fixes.sql

--- drizzle/migrations/0001_final_auth_fixes.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "uuid" TO "firebase_uid";
-ALTER TABLE "users" ADD CONSTRAINT "users_firebase_uid_unique" UNIQUE("firebase_uid");
-
-ALTER TABLE "delivery_boys" RENAME COLUMN "uuid" TO "firebase_uid";
-ALTER TABLE "delivery_boys" ADD CONSTRAINT "delivery_boys_firebase_uid_unique" UNIQUE("firebase_uid");
