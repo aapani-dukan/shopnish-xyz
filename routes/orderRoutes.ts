0294b7067342cba36fdf149ac896d30a9bfd361d
aapani-dukan <shivraj8404@gmail.com>
2025-08-19 14:59:06 +0530

    Update orderRoutes.ts

--- routes/orderRoutes.ts
+++ routes/orderRoutes.ts
@@ -1,14 +1,15 @@
 //routes/orderRoutes.ts
 
 import { Router } from 'express';
-import { createOrder, getUserOrders } from '../server/controllers/orderController.ts';
+import { placeOrder, getUserOrders } from '../server/controllers/orderController.ts';
 import { requireAuth } from '../server/middleware/authMiddleware.ts'; // ✅ मिडलवेयर को आयात करें
 
 const ordersRouter = Router();
 
 // POST और GET रूट्स पर `requireAuth` मिडलवेयर लागू करें
 // यह सुनिश्चित करता है कि अनुरोध भेजने से पहले उपयोगकर्ता प्रमाणित हो
-ordersRouter.post('/', requireAuth, createOrder);
+ordersRouter.post('/', requireAuth, placrOrder);
 ordersRouter.get('/', requireAuth, getUserOrders);
+
 
 export default ordersRouter;
