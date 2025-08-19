f63f4edc3c085fb2c7b7c1e1e40f8983c332878e
aapani-dukan <shivraj8404@gmail.com>
2025-08-19 15:11:04 +0530

    Update orderRoutes.ts

--- routes/orderRoutes.ts
+++ routes/orderRoutes.ts
@@ -8,7 +8,7 @@ const ordersRouter = Router();
 
 // POST और GET रूट्स पर `requireAuth` मिडलवेयर लागू करें
 // यह सुनिश्चित करता है कि अनुरोध भेजने से पहले उपयोगकर्ता प्रमाणित हो
-ordersRouter.post('/', requireAuth, placrOrder);
+ordersRouter.post('/', requireAuth, placeOrder);
 ordersRouter.get('/', requireAuth, getUserOrders);
 
 
