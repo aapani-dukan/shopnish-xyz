d135dce9b213b2358e8547fa3476fd2d31e75ee7
aapani-dukan <shivraj8404@gmail.com>
2025-08-25 14:54:39 +0530

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -2,12 +2,18 @@ import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "../shared/backend/schema.ts";
 import "dotenv/config";
-
+import { Client } from 'pg';
 // ✅ Ensure DATABASE_URL is defined
 if (!process.env.DATABASE_URL) {
   throw new Error("❌ DATABASE_URL must be set in your .env file");
 }
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
 
+await client.connect();
+
+export const db = drizzle(client, { schema }); // यहाँ पर schema ऑब्जेक्ट को पास किया गया है
 // ✅ PostgreSQL Pool with SSL support (for Render)
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
