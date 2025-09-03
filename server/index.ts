ca16843f2ff80bb6d7977f6092dceec22132d7cb
aapani-dukan <shivraj8404@gmail.com>
2025-09-03 21:04:44 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 
 // ✅ Socket helper import
-import { setIO } from "./socket.ts";
+import { getIO } from "./socket.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
