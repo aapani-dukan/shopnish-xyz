802d27ec89e7f9117e774b12556541c3ea3262db
aapani-dukan <shivraj8404@gmail.com>
2025-09-14 18:29:41 +0530

    Update DeliveryDashboard.tsx

--- client/src/pages/DeliveryDashboard.tsx
+++ client/src/pages/DeliveryDashboard.tsx
@@ -14,7 +14,7 @@ import {
 import DeliveryOtpDialog from "./DeliveryOtpDialog";
 import DeliveryOrdersList from "./DeliveryOrdersList";
 import { useAuth } from "@/hooks/useAuth";
-import { useSocket } from "@/providers/SocketProvider";
+import { useSocket } from "@/hooks/useSocket";
 import { apiRequest } from "@/lib/queryClient";
 import api from "@/lib/api";
 
