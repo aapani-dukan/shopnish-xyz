0caae9a5c38ab7f0fc7a0a3999cb115cdd37fc6d
aapani-dukan <shivraj8404@gmail.com>
2025-09-23 22:03:57 +0530

    Update firebase.ts

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -15,9 +15,9 @@ import {
 // ✅ Firebase config with auto key selection
 const firebaseConfig = {
   apiKey:
-    VITE_FIREBASE_API_KEY_APP = "AIzaSyAq2GbFOiMEedmLjp4ZxhwZ4I6OF2vp_DE",
-      
-    VITE_FIREBASE_API_KEY_WEB = "AIzaSyChdYrxfxkfj6m04WT0nOBl5xCP62udcPU",
+    import.meta.env.VITE_BUILD_TARGET === "app"
+    ? "AIzaSyAq2GbFOiMEedmLjp4ZxhwZ4I6OF2vp_DE" // Mobile / App key
+    : import.meta.env.VITE_FIREBASE_API_KEY,      // Web key
   authDomain: "aapani-dukan.firebaseapp.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
